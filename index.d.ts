61942f649b10bca89c0200783a8eec83f4780768
ithunter101 <ittechhunter513@gmail.com>
2022-08-10 00:07:11 +0300

    add type for createServer

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,9 @@ import {
   PoolConnection as PromisePoolConnection
 } from './promise';
 
+
+import Server from './lib/server.js';
+
 import * as mysql from './typings/mysql';
 export * from './typings/mysql';
 
@@ -199,3 +202,5 @@ export interface PoolOptions extends mysql.PoolOptions, ConnectionOptions {}
 export function createConnection(connectionUri: string): Connection;
 export function createConnection(config: ConnectionOptions): Connection;
 export function createPool(config: PoolOptions): Pool;
+
+export function createServer(handler: (conn: Connection) => any): Server;
